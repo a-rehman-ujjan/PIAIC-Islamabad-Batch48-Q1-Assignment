f2c935528dbaa2b0361da1408ac987a918fea057
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 19:55:52 +0500

    Create Q20.ts

---
+++ Q20.ts
@@ -0,0 +1,5 @@
+// Q20 - Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items
+
+let listofprogramminglanguages = ["JavaScript", "Python", "Java", "C++", "C#", "Ruby", "PHP", "Swift", "Go", "TypeScript", "Kotlin", "Rust", "MATLAB", "R", "Perl", "Objective-C", "Lua", "Bash", "PowerShell", "HTML/CSS", "SQL"]
+
+console.log(listofprogramminglanguages)
