83b22f0def3043bb38043a4f10a77bc53ead31eb
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 20:09:57 +0500

    Create Q35.ts

---
+++ Q35.ts
@@ -0,0 +1,11 @@
+// Q35 - Animals
+
+let animals = ["Dog", "Cat", "Rabbit"]
+for (let i = 0; i < animals.length; i++) {
+  console.log(animals[i]);
+}
+for (let i = 0; i < animals.length; i++) {
+  console.log("A " + animals[i] + " would make a great pet.");
+}
+
+console.log("Any of these animals would make a great pet!");
