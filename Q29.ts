3fe09fb0c66a1730ad680268982b33425871b592
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 20:06:42 +0500

    Create Q29.ts

---
+++ Q29.ts
@@ -0,0 +1,19 @@
+// Q29 - Favorite Fruit
+
+let favorite_fruits = ["watermelon", "apple", "peach"]
+
+if (favorite_fruits.indexOf("apple") != -1) {
+  console.log("You really like Apples!")
+}
+if (favorite_fruits.indexOf("peach") != -1) {
+  console.log("You really like Peaches!")
+}
+if (favorite_fruits.indexOf("bananas") != -1) {
+  console.log("You really like Bananas!")
+}
+if (favorite_fruits.indexOf("watermelon") != -1) {
+  console.log("You really like Watermelons!")
+}
+if (favorite_fruits.indexOf("grapes") != -1) {
+  console.log("You really like Grapes!")
+}
