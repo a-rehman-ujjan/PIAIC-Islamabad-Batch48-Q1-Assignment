7b3b766ff13c63b9b8f86ebbd59d7e657bde9603
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 20:01:26 +0500

    Create Q24.ts

---
+++ Q24.ts
@@ -0,0 +1,30 @@
+// Q24 - More Conditional Test
+console.log("Q-24")
+
+const string1: string = "Hello";
+const string2: string = "World";
+console.log(string1 === string2); // false
+console.log(string1 !== string2); // true
+
+const text = "UJJAN";
+const lowercaseText = text.toLowerCase();
+console.log(lowercaseText === "ujjan"); // true
+console.log(lowercaseText === "UJJAN"); // false
+
+const number1: number = 5;
+const number2: number = 10;
+console.log(number1 === number2); // false
+console.log(number1 !== number2); // true
+console.log(number1 > number2); // false
+console.log(number1 < number2); // true
+console.log(number1 >= number2); // false
+console.log(number1 <= number2); // true
+
+const condition1 = true;
+const condition2 = false;
+console.log(condition1 && condition2); // false (true and false)
+console.log(condition1 || condition2); // true (true or false)
+
+const array = [1, 2, 3, 4, 5];
+console.log(array.indexOf(3) != -1); // true
+console.log(array.indexOf(6) != -1); // false // if == -1 means value doesn't includes in the array
