6ed074096884813ee1731fac361c20f9ec647c0b
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 19:53:46 +0500

    Update Q17.ts

--- Q17.ts
+++ Q17.ts
@@ -1,4 +1,11 @@
 // Q17 - Shrinking Guest List
+let guests = ["Afraz", "Javaid", "Haseeb", "Malik Hassan", "Aqib"]
+
+console.log("\nDear " + guests[0] + ",\n\nWe hope this message finds you well. We wanted to reach out and invite you to a small dinner gathering that we are hosting at our humble abode.\n\nDate: 12-july-2023\nTime: 8:00 pm\nLocation: 7th sky Rooftop, 7th avanue Islamabad.\n")
+console.log("Dear " + guests[1] + ",\n\nWe hope this message finds you well. We wanted to reach out and invite you to a small dinner gathering that we are hosting at our humble abode.\n\nDate: 12-july-2023\nTime: 8:00 pm\nLocation: 7th sky Rooftop, 7th avanue Islamabad.\n")
+console.log("Dear " + guests[2] + ",\n\nWe hope this message finds you well. We wanted to reach out and invite you to a small dinner gathering that we are hosting at our humble abode.\n\nDate: 12-july-2023\nTime: 8:00 pm\nLocation: 7th sky Rooftop, 7th avanue Islamabad.\n")
+console.log("Dear " + guests[3] + ",\n\nWe hope this message finds you well. We wanted to reach out and invite you to a small dinner gathering that we are hosting at our humble abode.\n\nDate: 12-july-2023\nTime: 8:00 pm\nLocation: 7th sky Rooftop, 7th avanue Islamabad.\n")
+console.log("Dear " + guests[4] + ",\n\nWe hope this message finds you well. We wanted to reach out and invite you to a small dinner gathering that we are hosting at our humble abode.\n\nDate: 12-july-2023\nTime: 8:00 pm\nLocation: 7th sky Rooftop, 7th avanue Islamabad.\n")
 
 console.log("\n\nDue to unforeseen circumstances, we have encountered a delay with the delivery of the new dinner table we had planned to accommodate additional guests\n\nWe have space for only two guests")
 console.log("\n\nDear " + guests.shift() + ", We’re sorry you can’t invite them to dinner\n\n")
