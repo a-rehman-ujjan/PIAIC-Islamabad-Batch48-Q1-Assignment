4a07a1d7da552231e6317c64491563eb01786ceb
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 20:11:45 +0500

    Create Q37.ts

---
+++ Q37.ts
@@ -0,0 +1,7 @@
+// Q37 - Large Shirts
+
+function make_shirt(size = "Large", message = "I love TypeScript") {
+  console.log("The Shirt with message (" + message + ") Printed on it is the Size: " + size)
+}
+make_shirt()
+make_shirt("Medium", "There Is No Place Like 127.0.0.1")
