87234fd0c229acff3a81ed957a077f4821ff26e1
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 19:42:10 +0500

    Create Q3.ts

---
+++ Q3.ts
@@ -0,0 +1,10 @@
+// Q3 - Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+console.log(person.toLowerCase())
+console.log(person.toUpperCase())
+function titleCase(txt: any) {
+  let txttostring = txt.toString().toLowerCase()
+  txt = txttostring.charAt(0).toUpperCase() + txttostring.slice(1)
+  console.log(txt)
+}
+titleCase(person)
