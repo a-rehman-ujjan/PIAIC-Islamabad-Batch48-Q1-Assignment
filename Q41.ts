3dac80f789d75ad9f96a460eedae62c244ba07f6
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 20:16:46 +0500

    Create Q41.ts

---
+++ Q41.ts
@@ -0,0 +1,9 @@
+// Q41 - Magicians
+
+function show_magicians(names_array) {
+  names_array.forEach((x) => {
+    console.log(x)
+  })
+}
+let magicians = ["magician 1", "magician 2", "magician 3", "magician 4", "magician 5"]
+show_magicians(magicians)
