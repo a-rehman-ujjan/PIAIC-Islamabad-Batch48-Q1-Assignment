1a4b8a4f753604eb768a70bb2c99858919e10ae2
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 19:44:27 +0500

    Create Q7.ts

---
+++ Q7.ts
@@ -0,0 +1,7 @@
+// Q7 - Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+let sum = 3 + 5;
+let sub = 13 - 5;
+let product = 2 * 4;
+let division = 600 / 75;
+console.log(sum, sub, product, division)
