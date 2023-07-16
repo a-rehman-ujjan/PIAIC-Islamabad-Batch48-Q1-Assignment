5e0d4f64e5599628dec8cebf8829a0af1eff1a36
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 19:46:33 +0500

    Create Q10.ts

---
+++ Q10.ts
@@ -0,0 +1,7 @@
+// Q10 - Adding Comments
+
+let sum = 3 + 5; //this will add the two numbers
+let sub = 13 - 5; //this will sub the two numbers
+let product = 2 * 4; //this will multiply the two numbers
+let division = 600 / 75; //this will devide the two numbers
+console.log(sum, sub, product, division)
