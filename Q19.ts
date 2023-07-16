15d8d2d66c5b4fab854b306cb77aaddd29f64bed
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 19:54:54 +0500

    Create Q19.ts

---
+++ Q19.ts
@@ -0,0 +1,7 @@
+// Q19 - Dinner Guests
+
+guests = ["Afraz", "Javaid", "Haseeb", "Malik Hassan", "Aqib"]
+guests.unshift("Hassan ali")
+guests.splice(3, 0, "Rehan")
+guests.push("Faizan")
+console.log("number of people invited to dinner: " + guests.length)
