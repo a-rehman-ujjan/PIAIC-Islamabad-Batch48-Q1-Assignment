ae1c5e87367f3e19e235f3299a25a56dc90f842d
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 20:39:43 +0500

    Update Q22.ts

--- Q22.ts
+++ Q22.ts
@@ -1,6 +1,6 @@
 // Q22 - Intentional Error
 
-guests = ["Afraz", "Javaid", "Haseeb", "Malik Hassan", "Aqib"]
+let guests = ["Afraz", "Javaid", "Haseeb", "Malik Hassan", "Aqib"]
 console.log(guests)
 console.log(guests[4]) //will print the 5th element without any index error
 console.log(guests[8]) // will try to pring 9th element of the array which doesn't exist, causing an array index error
