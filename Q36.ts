b2b43cab3feda22181ae034afe32f156c457a6d7
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 20:10:50 +0500

    Create Q36.ts

---
+++ Q36.ts
@@ -0,0 +1,6 @@
+// Q36 - T-Shirt
+
+function make_shirt(size, message){
+  console.log("The Shirt with message ("+message+") Printed on it is the Size: "+size)
+}
+make_shirt(44, "There Is No Place Like 127.0.0.1")
