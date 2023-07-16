702d201be6b2c183221962883b718085009ed9c3
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 19:40:10 +0500

    Create Q2.ts

---
+++ Q2.ts
@@ -0,0 +1,4 @@
+// Q2 - Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+let person0 = "Ujjan";
+console.log(`Hello ${person0}, Welcome to PIAIC lets Learn & Earn!`);
