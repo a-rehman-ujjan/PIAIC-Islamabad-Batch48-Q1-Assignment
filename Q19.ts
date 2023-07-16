68f3328ab26fb6bf4f5f7707aa9b9eceb0d85c5d
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 20:39:02 +0500

    Update Q19.ts

--- Q19.ts
+++ Q19.ts
@@ -1,6 +1,6 @@
 // Q19 - Dinner Guests
 
-guests = ["Afraz", "Javaid", "Haseeb", "Malik Hassan", "Aqib"]
+let guests = ["Afraz", "Javaid", "Haseeb", "Malik Hassan", "Aqib"]
 guests.unshift("Hassan ali")
 guests.splice(3, 0, "Rehan")
 guests.push("Faizan")
