0c84d4a93ddfeab15761315ca0fdacb5b8808eca
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 20:14:30 +0500

    Create Q39.ts

---
+++ Q39.ts
@@ -0,0 +1,8 @@
+// Q39 - City names
+
+function city_country(city: string, country: string) {
+  console.log("\"" + city + "," + country + " \"")
+}
+city_country("busan", "south Korea")
+city_country("london", "England")
+city_country("Jerusalem", "Palestine")
