1f6a8909ac6c8adfdf3537b1514c14f3efd8d3e4
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 20:09:39 +0500

    Create Q34.ts

---
+++ Q34.ts
@@ -0,0 +1,10 @@
+// Q34 - Pizzas
+
+let pizzas = ["Chicken Tikka Pizza", "Meat Pizza", "Cheese Pizza"]
+for (let i = 0; i < pizzas.length; i++) {
+  console.log(pizzas[i])
+}
+for (let i = 0; i < pizzas.length; i++) {
+  console.log("I Really Like " + pizzas[i] + ", I always order it when I am out With Friends")
+}
+console.log("i don't consume fast food vary much but I really love pizza")
