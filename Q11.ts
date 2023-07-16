44f376ee81c2fa62b0c13ff1c7cf9f9f22ee2127
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 19:47:13 +0500

    Create Q11.ts

---
+++ Q11.ts
@@ -0,0 +1,8 @@
+// Q11 - Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+let names = ["Afraz", "Javaid", "Haseeb", "Hassan", "Aqib"]
+console.log(names[0])
+console.log(names[1])
+console.log(names[2])
+console.log(names[3])
+console.log(names[4])
