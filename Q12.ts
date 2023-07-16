479d91d76abae4a40e58cb6855064c0f6c1c7360
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 19:47:41 +0500

    Create Q12.ts

---
+++ Q12.ts
@@ -0,0 +1,8 @@
+// Q12 - Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let names = ["Afraz", "Javaid", "Haseeb", "Hassan", "Aqib"]
+console.log("There was an important saminar going on, I invited " + names[0] + " to come with me")
+console.log("There was an important saminar going on, I invited " + names[1] + " to come with me")
+console.log("There was an important saminar going on, I invited " + names[2] + " to come with me")
+console.log("There was an important saminar going on, I invited " + names[3] + " to come with me")
+console.log("There was an important saminar going on, I invited " + names[4] + " to come with me")
