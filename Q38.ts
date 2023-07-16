8ebfbccfef6557d279d498da21c5b8dd51972402
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 20:13:12 +0500

    Create Q38.ts

---
+++ Q38.ts
@@ -0,0 +1,8 @@
+// Q38 - Cities
+
+function describe_city(city, country = "Pakistan") {
+  console.log(city + " Is Located in " + country)
+}
+describe_city("Islamabad")
+describe_city("peshawar")
+describe_city("dubai", "UAE")
