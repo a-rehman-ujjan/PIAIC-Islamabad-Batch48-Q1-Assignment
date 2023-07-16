2152898e1209238c4304dc375dea98b60429665e
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 19:48:28 +0500

    Create Q13.ts

---
+++ Q13.ts
@@ -0,0 +1,6 @@
+// Q13 - Your Own Array
+
+let fvrts = ["125", "Corolla Grande", "Mercedes CLS63"]
+console.log("In Pakistan my fevorite moterbike is only Honda " + fvrts[0] + "cc.")
+console.log(fvrts[1] + " Is the only car i admired and wanted to buy once since i was a kid.")
+console.log("My dream sedan is " + fvrts[2])
