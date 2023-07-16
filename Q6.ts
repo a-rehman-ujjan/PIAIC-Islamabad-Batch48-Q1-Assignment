ddb792022e3f3755232794f2ba228d35e5a6da50
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 19:44:03 +0500

    Create Q6.ts

---
+++ Q6.ts
@@ -0,0 +1,6 @@
+// Q6 - Store a person’s name, and include some whitespace characters at the beginning and end of the namehen print the name after striping the white spaces.
+
+let name = "\t    \n    Ujjan       "
+console.log(name)
+name = name.trim()
+console.log(name)
