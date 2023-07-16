e35f03372686eb9e760172aefe5b9a95741c0541
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 19:44:47 +0500

    Create Q8.ts

---
+++ Q8.ts
@@ -0,0 +1,3 @@
+// Q8 - You should create four lines that look like this: console.log(5 + 3) Your output should simply be four lines with the number 8 appearing once on each line.
+
+console.log(`consol.log(3+5) = 8\nsconsol.log(13-5) = 8\nconsol.log(2*4) = 8\nconsol.log(600/75) = 8`)
