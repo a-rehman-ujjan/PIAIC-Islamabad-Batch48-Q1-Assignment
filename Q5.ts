9bfcd9ec45a552ca9b2c365ca5c763e15766f32d
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 19:43:21 +0500

    Create Q5.ts

---
+++ Q5.ts
@@ -0,0 +1,5 @@
+// Q5 - Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person = "Donald Trump"
+let message = famous_person + " once said,\"If you are not prepared to walk away, you can't negotiate.\""
+console.log(message)
