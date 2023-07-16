4383d0aa068cc9c10bd67142d56260bcef1f4787
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-07-16 20:04:40 +0500

    Create Q26.ts

---
+++ Q26.ts
@@ -0,0 +1,21 @@
+// Q26 - Alien Colors #2
+let alien_color = "green"
+
+// If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+
+if (alien_color == "green") {
+  console.log("Player just earned 5 points")
+}
+
+// If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+
+if (alien_color != "green") {
+  console.log("The player just earned 10 points.")
+}
+
+if (alien_color == "green") {
+  console.log("Player just earned 5 points")
+}
+else {
+  console.log("The player just earned 10 points.")
+}
